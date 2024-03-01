de075c0b27a8bb1ba0dadee06bf338f668b9ab04
Will Robertson <wspr81@gmail.com>
2024-03-01 20:40:05 +1030

    tweak example

--- sankey_example.py
+++ sankey_example.py
@@ -3,22 +3,32 @@ import pysankey as sky
 import matplotlib.pyplot as plt
 import pandas as pd
 
-colorDict =  {'a':'#f71b1b','b':'#1b7ef7','c':'#f3f71b','d':'#12e23f','orange':'#f78c1b'}
+colorDict =  {
+   'a':'#f71b1b',
+   'b':'#1b7ef7',
+  'ab':'#8821aa',
+  'ba':'#6016aa',
+  'cd':'#c1e849',
+   'c':'#f3f71b',
+   'd':'#12e23f',
+   'e':'#f78c1b',
+ }
 
 data = pd.DataFrame([
-  ("a",1.0,"a",2.0),
-  ("a",1.0,"b",0.8),
-  ("c",1.5,"c",0.5),
-  ("b",0.5,"b",0.8),
-  ("b",0.5,"a",0.8),
-  ("d",2.0,"c",0.4),
-  ])
+  ("a",1.0,"ab",2.0),
+  ("a",1.0,"ba",0.8),
+  ("c",1.5,"cd",0.5),
+  ("b",0.5,"ba",0.8),
+  ("b",0.5,"ab",0.8),
+  ("d",2.0,"cd",0.4),
+  ("e",1.0,"e",1.0),
+])
 
 sky.sankey(
   data,
   sorting = -1,
   colorDict = colorDict,
-  )
+)
   
 #plt.gcf().set_size_inches(6,6)
 # plt.savefig('fruits.png',bbox_inches='tight',dpi=150)
