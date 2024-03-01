a6214b2e8a98ea5a98b4c5d9619fe879f24ce6f3
Will Robertson <wspr81@gmail.com>
2024-03-01 21:24:05 +1030

    add three col example

--- sankey_example.py
+++ sankey_example.py
@@ -35,3 +35,25 @@ sky.sankey(
 
 plt.show()
 
+
+data = pd.DataFrame([
+  ("a",1.0,"ab",2.0,"a",1.0),
+  ("a",1.0,"ba",0.8,"ba",0.4),
+  ("c",1.5,"cd",0.5,"d",2.0),
+  ("b",0.5,"ba",0.8,"ba",0.4),
+  ("b",0.5,"ab",0.8,"a",1.0),
+  ("d",2.0,"cd",0.4,"d",1.0),
+  ("e",1.0,"e",1.0,"e",1.0),
+])
+
+sky.sankey(
+  data,
+  sorting = -1,
+  colorDict = colorDict,
+  labelWidth = 0.2
+)
+  
+#plt.gcf().set_size_inches(6,6)
+# plt.savefig('fruits.png',bbox_inches='tight',dpi=150)
+
+plt.show()
