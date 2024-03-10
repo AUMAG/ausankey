d478180e248bdfaa9a7b17223450f31c4cfb211c
Will Robertson <wspr81@gmail.com>
2024-03-10 11:21:56 +1030

    try again

--- pysankey/tests/test_fruit_defaults.py
+++ pysankey/tests/test_fruit_defaults.py
@@ -2,12 +2,11 @@ import matplotlib.pyplot as plt
 from pysankey import sankey as sky
 from .test_fruit_setup import TestFruit
 
-print(dir(sky))
-
 class TestFruitDefault(TestFruit):
 
     def test_fruits_default(self):
         
         plt.figure(dpi=150)
+        print(dir(sky))
         sky.sankey(self.data)
         plt.savefig("fruits_default.png")
