8976a14f2ebb3df77c79a94a6df8f1517fb4f376
Will Robertson <wspr81@gmail.com>
2024-03-10 13:43:17 +1030

    try some new tests

--- pysankey/tests/test_fruit_defaults.py
+++ pysankey/tests/test_fruit_defaults.py
@@ -5,7 +5,23 @@ from .test_fruit_setup import TestFruit
 class TestFruitDefault(TestFruit):
 
     def test_fruits_default(self):
-        
+
         plt.figure(dpi=150)
         sky.sankey(self.data)
         plt.savefig("fruits_default.png")
+
+    def test_fruits_sorting(self):
+        
+        plt.figure(dpi=150)
+        sky.sankey(self.data, sorting=1)
+        plt.savefig("fruits_sorting_1.png")
+        
+        plt.figure(dpi=150)
+        sky.sankey(self.data, sorting=-1)
+        plt.savefig("fruits_sorting_2.png")
+
+    def test_fruits_colormap(self):
+        
+        plt.figure(dpi=150)
+        sky.sankey(self.data, colormap="jet")
+        plt.savefig("fruits_colormap_jet.png")
