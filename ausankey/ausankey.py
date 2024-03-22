327a0e7bbc7a43134916078ea4ec1b46b14a0df9
Will Robertson <wspr81@gmail.com>
2024-03-22 21:58:19 +1030

    trim dead code

--- ausankey/ausankey.py
+++ ausankey/ausankey.py
@@ -386,10 +386,6 @@ def _sankey(
         ]
     else:
         bar_lr = [
-            weighted_sort(labels_lr[0], weights_lr[0], sorting),
-            weighted_sort(labels_lr[1], weights_lr[1], sorting),
-        ]
-        nodes_lr = [
             sort_nodes(labels_lr[0],node_sizes[ii]),
             sort_nodes(labels_lr[1],node_sizes[ii+1]),
         ]
@@ -627,40 +623,6 @@ def sort_dict(lbl, sorting):
         
     sort_arr = sorted(
         list(lbl.items()),
-        key=lambda item: s * item[1],
-        # sorting = 0,1,-1 affects this
-    )
-
-    sorted_labels = list(dict(sort_arr))
-
-    if sorting == "center":
-        # this kinda works but i dont think it's a good idea because you lose perception of relative sizes
-        # probably has an off-by-one even/odd error
-        sorted_labels = sorted_labels[1::2] + sorted_labels[-1::-2]
-
-    return sorted_labels
-
-
-###########################################
-
-def weighted_sort(lbl, wgt, sorting):
-    """creates a sorted list of labels by their summed weights"""
-
-    if sorting == "top":
-        s = 1
-    elif sorting == "bottom":
-        s = -1
-    elif sorting == "center":
-        s = 1
-    else:
-        s = 0
-
-    arr = {}
-    for uniq in lbl.unique():
-        arr[uniq] = wgt[lbl == uniq].sum()
-
-    sort_arr = sorted(
-        list(arr.items()),
         key=lambda item: s * item[1],
         # sorting = 0,1,-1 affects this
     )
